85e169c9c73564b4626125fd224a9bc786ccb32c
DONGBISULL <sdb7734@gmail.com>
2024-03-31 15:20:25 +0900

    프로파일 컴포넌트 생성

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,27 @@
-function Profile() {
+function Profile({ image, name, title, isNew }) {
+  // function Profile({ image, name, title, createTime }) {
   return (
-    <>
-      <h1>Profile</h1>
-    </>
+    <div className="profile">
+      {isNew ? <span className="newTag">NEW</span> : null}
+      {/* {isBeforeThreeDaysProfile(createTime) ? <span className="newTag">NEW</span> : null} */}
+      <img className="thumnail" src={image} alt="avatar" />
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
   );
+}
+
+//
+function isBeforeThreeDaysProfile(createTime) {
+  if (!createTime) return false;
+  let now = new Date();
+  let diff = now.getTime() - new Date(createTime).getTime();
+  // 3일 차이
+  let threeDaysInMillis = 3 * 24 * 60 * 60 * 1000;
+  if (diff <= threeDaysInMillis) {
+    return true;
+  }
+  return false;
 }
 
 export default Profile;
