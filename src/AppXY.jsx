85e43a901de36acce330b813620c26696b6232a8
DONGBISULL <sdb7734@gmail.com>
2024-11-03 16:36:48 +0900

    리액트 기본 코드 정리 / 심화 코드 세팅

---
+++ src/AppXY.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import "./AppXY.css";
+export default function AppXY() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handlePointerMove = (e) => {
+    // setPosition({x :  e.clientX , y : e.clientY});
+    // 수평이동만 가능할 경우
+    setPosition((pre) => ({
+      ...pre,
+      x: e.clientX,
+    }));
+  };
+
+  return (
+    <div className="container" onPointerMove={handlePointerMove}>
+      <div
+        className="pointer"
+        style={{ transform: `translate(${position.x}px , ${position.y}px)` }}
+      ></div>
+    </div>
+  );
+}
